42b9c2700c476b3d655d81c458a8c6ba6e5d7f5f
phillyjack <30764065+phillyjack@users.noreply.github.com>
2022-10-24 14:29:23 -0400

    Add files via upload

---
+++ restart-detect.ps1
@@ -0,0 +1,8 @@
+﻿$Uptime= get-computerinfo | Select-Object OSUptime 
+if ($Uptime.OsUptime.Days -ge 12){
+    Write-Output "Device has not rebootet on $($Uptime.OsUptime.Days) days, notify user to reboot"
+    Exit 1
+}else {
+    Write-Output "Device has rebootet $($Uptime.OsUptime.Days) days ago, all good"
+    Exit 0
+}
